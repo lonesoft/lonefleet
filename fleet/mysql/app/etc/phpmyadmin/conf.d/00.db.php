daad582813093da64b9b7c4b0ddb3f418d1c5b4f
Lorenzo <lorenzomengon@gmail.com>
2025-01-27 05:42:35 +0000

    mysql: First working version

---
+++ fleet/mysql/app/etc/phpmyadmin/conf.d/00.db.php
@@ -0,0 +1,10 @@
+<?php
+
+$cfg['Servers'][1] = [
+    'verbose' => 'mysql.lone',
+    'auth_type' => 'config',
+    'host' => 'db',
+    'user' => 'root',
+    'password' => 'Pc4lonetina!',
+    'compress' => true,
+];
